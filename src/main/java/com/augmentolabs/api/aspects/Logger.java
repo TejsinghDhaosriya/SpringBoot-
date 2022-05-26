4c0bd7cde474d7d54e805e7fde830efd52edc143
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-05-26 18:08:04 +0530

    aspect logger

---
+++ src/main/java/com/augmentolabs/api/aspects/Logger.java
@@ -0,0 +1,46 @@
+package com.augmentolabs.api.aspects;
+
+
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@Log4j2
+public class Logger {
+
+    private static final String POINTCUT = "within(com.augmentolabs.api.controllers.*)";
+
+    @Around(POINTCUT)
+    @SneakyThrows
+    public Object logAroundExecution(ProceedingJoinPoint pjp) {
+        log.info("before {}", constructLogMsg(pjp));
+        var proceed = pjp.proceed();
+        log.info("after {} with result: {}",constructLogMsg(pjp), proceed.toString());
+        return proceed;
+    }
+
+    @AfterThrowing(pointcut = POINTCUT, throwing = "e")
+    public void logAfterException(JoinPoint jp, Exception e) {
+        log.error("Exception during: {} with ex: {}", constructLogMsg(jp),  e.toString());
+    }
+
+    private String constructLogMsg(JoinPoint jp) {
+        var args = Arrays.stream(jp.getArgs()).map(String::valueOf).collect(Collectors.joining(",", "[", "]"));
+        Method method = ((MethodSignature) jp.getSignature()).getMethod();
+        return "@" + method.getName() +
+                ":" +
+                args;
+    }
+}
