dd65e3ccc071d53ee90b74dd352b811473792f5a
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-05-26 17:55:06 +0530

    format

--- src/main/java/com/augmentolabs/api/controllers/SearchController.java
+++ src/main/java/com/augmentolabs/api/controllers/SearchController.java
@@ -1,14 +1,14 @@
 package com.augmentolabs.api.controllers;
 
 import com.augmentolabs.api.facade.FacilityFacade;
-import com.augmentolabs.api.services.FacilityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.augmentolabs.api.utils.utils.getDate;
 
@@ -21,14 +21,14 @@ public class SearchController {
     private FacilityFacade facilityFacade;
 
     @GetMapping("/search")
-    public ResponseEntity getSearch(@RequestParam(value = "facilityName", required=false)  String facilityName,
-                                    @RequestParam(value = "startDate", required=false) String startDate,
-                                    @RequestParam(value = "endDate", required=false) String endDate,
-                                    @RequestParam(value = "informationAt", required=false) String informationAt
-                                    ){
-        try{
-            return new ResponseEntity(facilityFacade.search(facilityName,getDate(startDate),getDate(endDate),informationAt), HttpStatus.OK);
-        }catch(Exception e){
+    public ResponseEntity getSearch(@RequestParam(value = "facilityName", required = false) String facilityName,
+                                    @RequestParam(value = "startDate", required = false) String startDate,
+                                    @RequestParam(value = "endDate", required = false) String endDate,
+                                    @RequestParam(value = "informationAt", required = false) String informationAt
+    ) {
+        try {
+            return new ResponseEntity(facilityFacade.search(facilityName, getDate(startDate), getDate(endDate), informationAt), HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
